dddeb76b759cbb59f775a2274dc1b2debbf3ddac
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-30 15:01:42 +0300

    toastify updated

--- src/utils/toastify.js
+++ src/utils/toastify.js
@@ -1,2 +1,17 @@
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
+import "react-toastify/dist/ReactToastify.css";
+
+const Toastify = (msg) => {
+  toast.success(msg, {
+    position: "top-right",
+    hideProgressBar: false,
+    autoClose: 3000,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+export default toastify;
