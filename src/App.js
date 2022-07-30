7c0a6e319a7b36a1847d9348546172462feb1154
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-30 14:52:12 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './App.css';
+import Contacts from './components/contacts/Contacts';
+import FormComponent from './components/form/FormComponent';
+import {AddUser, UpdateUser} from "./utils/function"
+
+const initialValues ={username:"", phoneNumber:"", gender:""}
+
+function App() {
+
+const [info, setInfo] = useState(initialValues)
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  if (info.id){
+    UpdateUser(info);
+  }else{
+    AddUser(info);
+  }
+  setInfo(initialValues)
+}
+
+const editUser = (id, username, phoneNumber, gender) => {
+  setInfo({id, username, phoneNumber, gender});
+
+}
+
+  return (
+    <div className="App">
+      <FormComponent info={info} setInfo={setInfo} handleSubmit = {handleSubmit}/>
+      <Contacts editUser={editUser}/>
+    </div>
+  );
+}
+
+export default App;
